bbd980e0fd7aad847bfcfe0fd19ba3cc3df4cb7c
Vidhi  Sontakke <sontakkevidhi7@gmail.com>
2026-01-07 21:50:51 +0530

    Add files via upload

---
+++ GuessGame.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+import java.util.Random;
+class GuessGame{
+    public static void main(String[] args) {
+         
+        Scanner sc = new Scanner(System.in);
+        Random rand = new Random();
+
+        int computerNumber = 
+
+
+    }
+}
+
+
+
+    
+}
